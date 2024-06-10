ce94a42b84c03dbeb28284a4d72e0d25210dce8d
PRAVEENKUMARRAJENDRAN <praveenrajendran2109@gmail.com>
2024-06-10 19:43:50 -0400

    Excerise 2

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,4 +1,26 @@
+"""
+Time complexity: O(1)
+Space complexity: O(1)
 
+Any problem you faced while coding this:
+
+Popping when single element was bit challenging.
+
+Explantion:
+Created a top element that is head in the linked list.
+
+When pushing checked the top if it is None, updated the top element
+or else made new node as top element 
+
+Used two condition, 
+
+When more than one node is available
+Made the top node next as Top element.
+
+When one Node was available 
+Made the top as None 
+
+"""
 class Node:
     def __init__(self, data):
        self.data = data
@@ -6,12 +28,43 @@ class Node:
  
 class Stack:
     def __init__(self):
+        self.top = None
         
     def push(self, data):
+        temp = Node(data)
+        if self.top == None:
+            self.top = temp
+        else:
+            temp.next = self.top
+            self.top = temp
         
-    def pop(self):
+    def pop(self):      
+        if self.top and self.top.next:
+            temp = self.top 
+            self.top = self.top.next 
+            temp.next = None
+            return temp.data  
+        elif self.top and not self.top.next:
+            temp = self.top 
+            self.top = None
+            return temp.data
+        else:
+            return None
+
+
+        
         
 a_stack = Stack()
+
+#a_stack.push(1)
+#a_stack.push(2)
+#a_stack.push(3)
+#print(a_stack.pop())
+#print(a_stack.pop())
+#print(a_stack.pop())
+#print(a_stack.pop())
+
+
 while True:
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
     print('push <value>')
@@ -30,3 +83,6 @@ while True:
             print('Popped value: ', int(popped))
     elif operation == 'quit':
         break
+        
+
+
